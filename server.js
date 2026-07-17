e616eac44da8081496ae9e22dbd26b610d404702
LoganPSI <logan.t@psigroup.net.au>
2026-07-17 15:12:48 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ const pool = new Pool({
 const DASHBOARD_PASSCODE = process.env.DASHBOARD_PASSCODE || 'psi2026';
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-passcode');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Create the table on startup if it doesn't already exist
